2cd6ec14e245f49c17419cd17cd6d7ea547cd5c0
sebatian003peru <sebastian003peru@gmail.com>
2017-10-30 18:10:57 -0500

    Added Controller

---
+++ controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MyInventarySc {
+
+	bool Selected;
+	bool click;
+
+	void Update () {
+
+		if (Input.GetMouseButton(0)){
+
+			Slots[indexImagenEquip].enabled=false;
+			Selected = true;
+		}
+			else if (Input.GetMouseButton(0)) {
+				Selected =false;
+
+			}
+
+		
+			
+		
+
+		if (Selected==true){
+			Slots[indexImagenEquip].enabled=false;
+		}
+
+		else if (Selected==false){
+			Slots[indexImagenEquip].enabled=true;
+		}
+		
+	}
+}

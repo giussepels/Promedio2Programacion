e88eff5aa0ee799c5d56e87455aba81babf3ad2b
LordalZero <lordalzero@gmail.com>
2017-10-30 17:32:47 -0500

    Bug fix interfaz grafica

---
+++ Assets/Scripts/MyInventarySc.cs
@@ -0,0 +1,14 @@
+﻿public abstract class MyInventarySc
+{
+	public Espadas _espadas;
+	public void TipoDeEspadas (Espadas espadas)
+	{
+		this._espadas = espadas;
+	}
+}
+public enum Espadas 
+{
+	EspadaDeFuego,
+	EspadaDeAire,
+	EspadaDeAgua
+}
